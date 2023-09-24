218dfee7ffc0bfc874aadba88f8a12fb5ff0f660
Michal Lorenc <Mlorenc11@protonmail.com>
2023-09-24 14:43:55 +0200

    Aded function to decrypt messeges without knowing the encryption key

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -10,7 +10,8 @@ def encrypt(message, key):
     for char in message:
         if char in letters:
             new_index = letters.index(char) + key
-            if new_index > 26:
+            # print(f"Char:{char}, Index:{letters.index(char)}, New index:{new_index}")
+            if new_index >= 26:
                 new_index -= 26
             output_text += letters[new_index]
         else:
@@ -27,12 +28,33 @@ def decrypt(message, key):
     for char in message:
         if char in letters:
             new_index = letters.index(char) - key
+
             if new_index < 0:
                 new_index += 26
+
             output_text += letters[new_index]
         else:
             output_text += char
     return output_text
+
+
+def crack(message):
+    letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 'M', "N", "O", 'P', 'Q', 'R', "S", "T", "U",
+               'V', 'W', 'X', 'Y', 'Z']
+    message = message.upper()
+    lst = []
+    for i in range(1, 27):
+        output_text = ""
+        for char in message:
+            if char in letters:
+                new_index = letters.index(char) - i
+                if new_index < 0:
+                    new_index += 26
+                output_text += letters[new_index]
+            else:
+                output_text += char
+        lst.append(output_text)
+    return lst
 
 
 def generate_html_response():
@@ -49,6 +71,10 @@ def generate_html_response():
                 <h1>Decryption</h1>
                 To decrypt a message go to: <b>http://127.0.0.1:8000/decrypt/?msg=YOUR MESSAGE GOES HERE&key=INPUT ENCRYPTION KEY HERE(1-25)</b><br>
                 exemple <a href="http://127.0.0.1:8000/decrypt/?msg=fgh&key=5">http://127.0.0.1:8000/decrypt/?msg=fgh&key=5</a>
+                
+                <h1>Decryption</h1>
+                To decrypt a message without knowing the key go to: <b>http://127.0.0.1:8000/crack/?msg=YOUR MESSAGE GOES HERE</b><br>
+                exemple <a href="http://127.0.0.1:8000/decrypt/?msg=fgh&key=5">http://127.0.0.1:8000/crack/?msg=fgh</a>
         </body>
     </html>
     """
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from typing import Annotated
-from Caesar_cipher import encrypt, decrypt, generate_html_response
+from Caesar_cipher import encrypt, decrypt, generate_html_response, crack
 
 from fastapi.responses import HTMLResponse
 from fastapi import FastAPI, Query
 
 app = FastAPI()
-
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -23,3 +22,10 @@ def encryption(msg: str, key: Annotated[int, Query(title="key for enncryption", 
 def decryption(msg: str, key: Annotated[int, Query(title="key for enncryption", ge=1, le=26)]):
     new_msg = decrypt(msg, key)
     return {"msg": msg, "new_msg": new_msg}
+
+
+@app.get("/crack/")
+def cracking(msg: str):
+    lst = crack(msg)
+    for idx, messege in enumerate(lst):
+        print(f"Index: {idx}--{messege}")
